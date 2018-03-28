afb724125d3584d7b26be2b0751e3608e26e8e68
Junyoung Kim <junkim327@gmail.com>
2018-03-27 23:54:52 -0500

    Add isPrime() method.

--- src/com/company/algorithms/easy/CountPrimes.java
+++ src/com/company/algorithms/easy/CountPrimes.java
@@ -88,7 +88,23 @@ public class CountPrimes {
     return c;
   }
 
+  private static boolean isPrime(int num) {
+    if (num < 2) {
+      return false;
+    }
+
+    for (int i = 2; i <= Math.sqrt(num); i++) {
+      if (num % i == 0) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
   public static void main(String[] args) {
     System.out.println(countPrimes(499979));
+    System.out.println(isPrime(4));
+    System.out.println(isPrime(37));
   }
 }
