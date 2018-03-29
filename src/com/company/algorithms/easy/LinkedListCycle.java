8ac76292569b5f760a7dbf7f0edff155a03b294a
Junyoung Kim <junkim327@gmail.com>
2018-03-28 22:41:32 -0500

    Add solution for "Linked List Cycle" problem.

--- src/com/company/algorithms/easy/LinkedListCycle.java
+++ src/com/company/algorithms/easy/LinkedListCycle.java
@@ -1,12 +1,12 @@
 package com.company.algorithms.easy;
 
-public class LinkedListCycle {
-  public boolean hasCycle(ListNode head) {
+public class LinkedListCycle<T> {
+  public boolean hasCycle(Node<T> head) {
     if (head == null) {
       return false;
     }
-    ListNode walker = head;
-    ListNode runner = head;
+    Node<T> walker = head;
+    Node<T> runner = head;
     while (runner.next != null && runner.next.next != null) {
       walker = walker.next;
       runner = runner.next.next;
@@ -15,6 +15,26 @@ public class LinkedListCycle {
       }
     }
     return false;
+  }
+
+  private static class Node<T> {
+    private T data;
+    private Node<T> next;
+
+    public Node(T data, Node<T> next) {
+      this.data = data;
+      this.next = next;
+    }
+  }
+
+  public static void main(String[] args) {
+    Node<Integer> node3 = new Node<>(7, null);
+    Node<Integer> node2 = new Node<>(5, node3);
+    Node<Integer> node = new Node<>(3, node2);
+    node3.next = node;
+
+    LinkedListCycle<Integer> ls = new LinkedListCycle<>();
+    System.out.println(ls.hasCycle(node));
   }
 }
 
