e40d397a948d60232394a8a3f5787699e3a9e26a
Junyoung Kim <junkim327@gmail.com>
2019-05-30 01:55:32 +0900

    Add "560. Subarray Sum Equal K" solution

---
+++ src/com/company/algorithms/medium/SubarraySumEqualsK.java
@@ -0,0 +1,20 @@
+package com.company.algorithms.medium;
+
+import java.util.HashMap;
+
+public class SubarraySumEqualsK {
+  public int subarraySum(int[] nums, int k) {
+    int count = 0;
+    int sum = 0;
+    HashMap<Integer, Integer> map = new HashMap<>();
+    map.put(0, 1);
+
+    for (int i = 0; i < nums.length; i++) {
+      sum += nums[i];
+      count += map.getOrDefault(sum - k, 0);
+      map.put(sum, map.getOrDefault(sum, 0) + 1);
+    }
+
+    return count;
+  }
+}
